b7b4c6e8167dc784efb95d11a4cb3b5784ea8d6d
GreenJoey <esyray15@gmail.com>
2018-02-05 15:13:55 +0530

    More Examples

---
+++ the coding guys/prog1.vb
@@ -0,0 +1,6 @@
+Module Program
+    Sub Main()
+        Console.WriteLine("My First Program")
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ the coding guys/variables.vb
@@ -0,0 +1,18 @@
+Module Variables
+    Sub Main()
+        ' Variables are decalred using `Dim <var name> As <type>`
+        Dim myNum As Integer
+        myNum = 5
+        Dim nam As String
+        nam = "Asim"
+
+        Dim x As Integer
+        Dim y As Integer
+        x = 32
+        y = 15
+
+        Console.WriteLine(nam)
+        Console.WriteLine(myNum)
+        Console.WriteLine(x+y)
+    End Sub
+End Module
