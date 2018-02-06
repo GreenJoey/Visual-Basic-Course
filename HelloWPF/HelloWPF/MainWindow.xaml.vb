13f1caaf5fc21dbfa99abf74532ca3af84d67d64
GreenJoey <esyray15@gmail.com>
2018-02-06 20:26:30 +0530

    A Hello World WPF Program

---
+++ HelloWPF/HelloWPF/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        If RadioButton1.IsChecked Then
+            MessageBox.Show("Hello.")
+        ElseIf RadioButton2.IsChecked Then
+            MessageBox.Show("Goodbye.")
+        End If
+    End Sub
+End Class
